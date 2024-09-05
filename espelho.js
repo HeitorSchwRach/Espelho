cd294fc1f226803b46ad2047568abb3cdb02c243
dkz <162715214+HeitorSchwRach@users.noreply.github.com>
2024-09-05 12:19:46 -0300

    Add files via upload

---
+++ espelho.js
@@ -0,0 +1,26 @@
+let foco = 0;
+let posicao = 0;
+let altura= 0;
+
+if(foco>0){
+    console.log("Espelho côncavo")
+}else if (foco=0){
+    console.log ("Espelho plano")
+}else{
+    console.log("Espelho convexo")
+}
+
+if(posicao>0){
+    console.log("Ok")
+}else{
+ console.log("Não pode")
+}
+
+if(altura>0){
+    console.log("Objeto para cima")
+}else if(altura=0){
+    console.log("Objeto não existe")
+}else{
+    console.log("Objeto para baixo")
+}
+
